212b631f91a5b5a697a5f5a892f9f98e64c3c1fd
yash <hsay0605@gmail.com>
2025-09-12 21:45:08 +0530

    Merge Sorted Array

---
+++ Practice/Misc/Merge Sorted Array/msa.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0)
+            return;
+        if (m == 0) {
+            int t = 0;
+            while (n > 0) {
+                nums1[t] = nums2[t];
+                t++;
+                n--;
+            }
+        }
+
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+}
